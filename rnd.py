04778f77b35afe16e3e015778231e7b6d35742d1
Iyamoto <Iyamoto@users.noreply.github.com>
2017-08-24 22:09:41 +0300

    fire added

--- rnd.py
+++ rnd.py
@@ -2,13 +2,18 @@
 
 import requests
 import time
+import fire
 
 url = 'http://127.0.0.1:8080'
 timeout = 1 # Seconds
 
 def check(url=url, timeout=timeout):
+    """Checks URL"""
     while True:
         r = requests.get(url)
         if not r.ok:
             return (r.status_code, r.reason)
         time.sleep(timeout)
+
+if __name__ == '__main__':
+  fire.Fire(check)
