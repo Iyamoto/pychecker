2137651a2c69481b128958000c567b44a7cc5fd4
Iyamoto <Iyamoto@users.noreply.github.com>
2017-08-24 22:19:15 +0300

    upload

--- rnd.py
+++ rnd.py
@@ -4,14 +4,23 @@ import requests
 import time
 import fire
 
-url = 'http://127.0.0.1:8080'
+url = 'http://127.0.0.1:80'
 timeout = 1 # Seconds
+requests.adapters.DEFAULT_RETRIES = 1
+reqesttimeout = 1 # Seconds
 
-def check(url=url, timeout=timeout):
-    """Checks URL"""
+def check(url=url, timeout=timeout, reqesttimeout=reqesttimeout):
+    """Checks URL
+    Args:
+        url (str): URL to check
+        timeout (int): Time between checks, seconds
+    Return:
+        """
     while True:
-        r = requests.get(url)
+        r = requests.get(url, timeout=reqesttimeout)
         if not r.ok:
+            return (r.status_code, r.reason)
+        else:
             return (r.status_code, r.reason)
         time.sleep(timeout)
 
