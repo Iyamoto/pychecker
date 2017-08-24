0264b0e94e838974a49d88caa10fcd2248e53cb4
Iyamoto <Iyamoto@users.noreply.github.com>
2017-08-24 22:39:36 +0300

    upload

--- rnd.py
+++ rnd.py
@@ -3,8 +3,9 @@
 import requests
 import time
 import fire
+import collections
 
-url = 'http://www.google.com:801'
+url = 'http://www.google.com:80'
 timeout = 1 # Seconds
 requests.adapters.DEFAULT_RETRIES = 1
 requesttimeout = 1 # Seconds
@@ -19,19 +20,23 @@ def check(url=url, timeout=timeout, requesttimeout=requesttimeout):
         """
     c = 0
     limit = 2
+    data = collections.defaultdict(int)
 
     while True:
         c += 1
         try:
             r = requests.get(url, timeout=requesttimeout)
         except requests.exceptions.RequestException as e:
-            # print(type(e).__name__)
-            return type(e).__name__
+            error = type(e).__name__
+            # print(error)
+            data[error] += 1
+            # return type(e).__name__
+
         if timeout > 0:
             time.sleep(timeout)
 
         if c > limit:
-            exit()
+            return data
 
 if __name__ == '__main__':
   fire.Fire(check)
